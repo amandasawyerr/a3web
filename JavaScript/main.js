5c7e1d7089af65d81654a872e5b750fa5644b7fc
amandasawyerr <114168681+amandasawyerr@users.noreply.github.com>
2022-12-08 17:53:25 -0500

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,55 @@
+// By Amanda 
+// External javascript file to be referenced on index and programming page
+// Functions defined in javascript file, to be called in main html file
+// All adapted from w3schools
+
+// Change text function
+// Changes html content upon event 
+function changeTxt() {
+  document.getElementById("javstxt").innerHTML = "Just kidding";
+}
+// Change color functions
+// Changes color of text upon event 
+function changeClr(x) {
+    x.style-color; 'yellow'
+}
+function normClr(x) {
+  x.style-color; 'white'
+}
+function normClr() {
+  style-color; 'white'
+}
+// Change text size function
+function bigTxt() {
+  document.getElementById('big').style.fontSize='100px';
+}
+// Alert functions
+// Causes alert upon event
+function alrt() {
+  alert("JavaScript alert")
+}
+function inpt() {
+  alert("Invalid address");
+}
+// Key value function
+// Change text to key value
+function catyrs(event) {
+  document.getElementById("cat").innerHTML = event.keyCode;
+}
+
+
+
+// By Josh
+// Alert function for Geovisualization Page
+function meow() {
+  alert("Have you seen my baseball?")
+}
+
+// Alert function for Spatial Analysis Page
+function meow() {
+  alert("Have you seen my Dad?")
+}
+
+
+
+
---
+++ README.md
@@ -0,0 +1,35 @@
+# a3web - Group 4 Project - 8 Page GIS Consulting Website
+
+# Distribution of workload
+# PLS mark with DONE upon completion 
+
+## Alex
+### 2 pages context/HTML - web and data - DONE
+### Aesthetic and consistency
+### Design of logo - DONE
+### Comments for contributions
+
+## Amanda
+### 2 pages context/HTML - home and program - DONE 
+### Javascript - its own file, in its own folder - DONE
+### Error checking - HTML/CSS/Java validators - DONE
+### Comments for contributions - DONE
+
+## Justin
+### 2 pages context/HTML - remosens and survey
+### Responsive page
+### Images - in there own file within repository
+### Comments for contributions
+
+## Josh
+### 2 pages context/HTML - geovis and span
+### jQuery - within Javascript file
+### Error checking - unused CSS, URLS, tags, spelling
+### Comments for contributions
+
+# Additional Testing Region - completion currently unconfirmed
+
+# NOTES from Shawn
+## Take away B tags - DO ON UR PAGES PLS
+## Comment of code ownership - EVERYONE ON THEIR PAGES PLS
+## Add jQuery to JS file
